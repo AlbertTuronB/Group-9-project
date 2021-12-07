90707db87a220984e0a009abd4899627ff3b4a4d
PaulMarkTammiste01 <95356219+PaulMarkTammiste01@users.noreply.github.com>
2021-12-07 12:56:10 +0000

    Update Group 9 project.py

--- Group 9 project.py
+++ Group 9 project.py
@@ -1,19 +1,21 @@
 from matplotlib import pyplot as plt
 
-# number of individuals susceptible to virus
-S = [999]
-# number of individuals infected to virus
-I = [1]
-# number of individuals not able to contract virus due to having been infected and developed an immunity or
-# died due to infection
-R = [0]
-# how many infected on average stem from one infected
-Ro = 0.5
-# average recovery time
-T = 12
-days = 365
+def main_function():
+    # number of individuals susceptible to virus
+    S = [999]
+    # number of individuals infected to virus
+    I = [1]
+    # number of individuals not able to contract virus due to having been infected and developed an immunity or
+    # died due to infection
+    R = [0]
+    # how many infected on average stem from one infected
+    Ro = 0.5
+    # average recovery time
+    T = 12
+    days = 100
+    Simulation_without_vaccine(S, I, R, Ro, T, days)
 
-def Simulation_without_vaccine():
+def Simulation_without_vaccine(S, I, R, Ro, T, days):
     for n in range(1, days):
         if S[n-1]/I[n-1]>= 1:
             S.append(S[n-1]-Ro*I[n-1])
@@ -24,7 +26,7 @@ def Simulation_without_vaccine():
             R.append(R[n - 1] + I[n - 1] / T)
             I.append(I[n - 1] + Ro * S[n - 1] * S[n - 1]/I[n - 1] - I[n - 1] / T)
     print(list(map(round,I)))
-    
+
     plt.plot(S, label="Susceptible")
     plt.plot(I, label="Infected")
     plt.plot(R, label="Recovered")
@@ -33,5 +35,5 @@ def Simulation_without_vaccine():
     plt.title("Virus simulation")
     plt.legend(loc="best")
     plt.show()
-    
-Simulation_without_vaccine()
+
+main_function()
