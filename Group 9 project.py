54cedea4c5a0435260506b9088d5f1ba46eb7e4e
PaulMarkTammiste01 <95356219+PaulMarkTammiste01@users.noreply.github.com>
2021-12-07 12:46:56 +0000

    Update Group 9 project.py

--- Group 9 project.py
+++ Group 9 project.py
@@ -13,22 +13,25 @@ Ro = 0.5
 T = 12
 days = 365
 
-for n in range(1, days):
-    if S[n-1]/I[n-1]>= 1:
-        S.append(S[n-1]-Ro*I[n-1])
-        R.append(R[n-1]+I[n-1]/T)
-        I.append(I[n-1]+Ro*I[n-1]-I[n-1]/T)
-    else:
-        S.append(S[n - 1] - Ro * S[n - 1] * S[n - 1]/I[n - 1])
-        R.append(R[n - 1] + I[n - 1] / T)
-        I.append(I[n - 1] + Ro * S[n - 1] * S[n - 1]/I[n - 1] - I[n - 1] / T)
-print(list(map(round,I)))
-
-plt.plot(S, label="Susceptible")
-plt.plot(I, label="Infected")
-plt.plot(R, label="Recovered")
-plt.xlabel("Days")
-plt.ylabel("Number of individuals")
-plt.title("Virus simulation")
-plt.legend(loc="best")
-plt.show()
+def Simulation_without_vaccine:
+    for n in range(1, days):
+        if S[n-1]/I[n-1]>= 1:
+            S.append(S[n-1]-Ro*I[n-1])
+            R.append(R[n-1]+I[n-1]/T)
+            I.append(I[n-1]+Ro*I[n-1]-I[n-1]/T)
+        else:
+            S.append(S[n - 1] - Ro * S[n - 1] * S[n - 1]/I[n - 1])
+            R.append(R[n - 1] + I[n - 1] / T)
+            I.append(I[n - 1] + Ro * S[n - 1] * S[n - 1]/I[n - 1] - I[n - 1] / T)
+    print(list(map(round,I)))
+    
+    plt.plot(S, label="Susceptible")
+    plt.plot(I, label="Infected")
+    plt.plot(R, label="Recovered")
+    plt.xlabel("Days")
+    plt.ylabel("Number of individuals")
+    plt.title("Virus simulation")
+    plt.legend(loc="best")
+    plt.show()
+    
+Simulation_without_vaccine()
