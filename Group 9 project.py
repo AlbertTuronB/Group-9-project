541e3317c49fe5733f728960b66f445246d01837
PaulMarkTammiste01 <95356219+PaulMarkTammiste01@users.noreply.github.com>
2021-12-07 13:53:06 +0000

    Update Group 9 project.py

--- Group 9 project.py
+++ Group 9 project.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 
-def maininput():
+def main_function():
+    
     S = 0
     I = 0
     Ro = 0
@@ -44,31 +45,13 @@ def maininput():
             continue
         
     print(S,I,Ro,R,T,days)
-
-
-
-#def main_function():
-    # number of individuals susceptible to virus
-    #S = [999]
-    # number of individuals infected to virus
-   # I = [1]
-    # number of individuals not able to contract virus due to having been infected and developed an immunity or
-    # died due to infection
-   # R = [0]
-    # how many infected on average stem from one infected
-   # Ro = 0.5
-    # average recovery time
-   # T = 12
-    # period of time over which the simulation is played
-   # days = 100
-    # % of susceptible individuals getting vaccinated every day
+    
     ActVac = 0.01
 
     Simulation_without_vaccine(S, I, R, Ro, T, days)
     Simulation_with_active_vaccinating(S, I, R, Ro, T, days, ActVac)
-    Restart()
-
-
+    Restart()    
+    
 def Simulation_without_vaccine(S, I, R, Ro, T, days):
     for n in range(1, days):
         if S[n - 1] / I[n - 1] >= 1:
